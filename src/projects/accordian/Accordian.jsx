4907a2bc2844131c78a2643871f758b27fe0eb57
ImranullahKhann <imranullahkhann2004@gmail.com>
2025-12-13 20:48:01 +0500

    Add project 1-accordian

--- src/projects/accordian/Accordian.jsx
+++ src/projects/accordian/Accordian.jsx
@@ -6,18 +6,34 @@ import { useState } from "react";
 
 export default function Accordian() {
   const [active, setActive] = useState(-1);
-  const [multipleSelec,setMultipleSelec] = useState(true);
+  const [multipleSelec, setMultipleSelec] = useState(false);
   const [multiple, setMultiple] = useState([]);
 
   return (
     <div className="accordian">
       <h2>Accordian</h2>
+      <button onClick={() => setMultipleSelec(!multipleSelec)}>
+        {multipleSelec ? "Disable" : "Enable"} Multi Selection
+      </button>
       <div>
-        { !multipleSelec ? 
-        data.map(item => <SingleSelec data={item} state={active} key={item.id} setState={setActive} />) 
-        :
-        data.map(item => <MultipleSelec data={item} multiple={multiple} key={item.id} setMultiple={setMultiple} />) }
+        {multipleSelec
+          ? data.map((item) => (
+              <MultipleSelec
+                data={item}
+                multiple={multiple}
+                key={item.id}
+                setMultiple={setMultiple}
+              />
+            ))
+          : data.map((item) => (
+              <SingleSelec
+                data={item}
+                state={active}
+                key={item.id}
+                setState={setActive}
+              />
+            ))}
       </div>
     </div>
-  )
+  );
 }
--- src/projects/accordian/components/MultipleSelec.jsx
+++ src/projects/accordian/components/MultipleSelec.jsx
@@ -1,27 +1,28 @@
-export default function MultipleSelec ({ data, multiple, setMultiple }) {
-  function handleClick (id) {
+export default function MultipleSelec({ data, multiple, setMultiple }) {
+  function handleClick(id) {
     const found = multiple.findIndex((elem) => id === elem);
 
-    if (found === -1)
-        setMultiple(multiple.push(id))
-    else    
-        setMultiple(multiple.splice(found, 1))
+    if (found === -1) setMultiple((prev) => [...prev, id]);
+    else setMultiple((prev) => prev.filter((elem) => elem !== id));
   }
 
-  function checkToggle (id) {
+  function checkToggle(id) {
     const found = multiple.findIndex((elem) => id === elem);
 
-    if (found === -1)
-        return false;
-    else 
-        return true;
+    if (found === -1) return false;
+    else return true;
   }
-  
+
   return (
     <div onClick={() => handleClick(data.id)} className="singleSelec">
-      <div className="question">{data.question}</div><span className="toggle">{ checkToggle(data.id) ? "-" : "+"}</span>
-      { checkToggle(data.id) ? <div className="answer">{ data.answer }</div> : <div></div> }
+      <div className="question">{data.question}</div>
+      <span className="toggle">{checkToggle(data.id) ? "-" : "+"}</span>
+      {checkToggle(data.id) ? (
+        <div className="answer">{data.answer}</div>
+      ) : (
+        <div></div>
+      )}
       <hr />
     </div>
-  )
-};
+  );
+}
